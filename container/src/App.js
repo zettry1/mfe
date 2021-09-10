8590d81d8f40c51dfd645b3d283c94a133e80766
orgil <orgilbat@jrcs.co.jp>
2021-09-10 21:55:42 +0900

    fix: domain name

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./component/MarketingApp";
 function App() {
   return (
     <div>
-      <h1>App container v1</h1>
+      <h1>App container dom name</h1>
       <MarketingApp />
     </div>
   );
