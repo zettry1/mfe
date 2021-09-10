183294fd918920e52b9eb740049e2f6811609959
orgil <orgilbat@jrcs.co.jp>
2021-09-10 19:54:32 +0900

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./component/MarketingApp";
+function App() {
+  return (
+    <div>
+      <h1>App container</h1>
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
