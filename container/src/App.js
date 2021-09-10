977007b4a6df84b795f186e55bd72f7b3a86ee49
orgil <orgilbat@jrcs.co.jp>
2021-09-10 21:14:24 +0900

    fix: app name

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./component/MarketingApp";
 function App() {
   return (
     <div>
-      <h1>App container v1</h1>
+      <h1>App container v11</h1>
       <MarketingApp />
     </div>
   );
